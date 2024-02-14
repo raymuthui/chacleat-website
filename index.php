ad3763a32ce0a486e4e97d47cb647e8f158aa899
Lennox Kabo <borelennox@gmail.com>
2024-02-15 02:55:23 +0300

    changes home page

--- index.php
+++ index.php
@@ -13,7 +13,46 @@
         ?>
     </header>
 
-    <main></main>
+    <main>
+        <section class="slideshow">
+
+        </section>
+        <section class="home-services-overview">
+            <div class="service-top">
+                <h2>OUR SERVICES</h2>
+                <h6>YEARS OF EXPERIENCE</h6>
+            </div>
+            <div class="service-bottom">
+                <div class="home-services">
+                    <div class="service-image-top"><img src="assets/images/construction-icon.png" alt="construction"></div>
+                    <div class="service-details">
+                        <h2>CONSTRUCTION</h2>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.</p>
+                    </div>
+                </div>
+                <div class="home-services">
+                    <div class="service-image-top"><img src="assets/images/roofing-icon.png" alt="roofing"></div>
+                    <div class="service-details">
+                        <h2>ROOFING</h2>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.</p>
+                    </div>
+                </div>
+                <div class="home-services">
+                    <div class="service-image-top"><img src="assets/images/remodeling-icon.png" alt="remodeling"></div>
+                    <div class="service-details">
+                        <h2>REMODELING</h2>
+                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="featured-work">
+
+        </section>
+        <section class="home-testimonials">
+
+        </section>
+    </main>
     <footer>
         <?php
             include_once 'footer.php';
