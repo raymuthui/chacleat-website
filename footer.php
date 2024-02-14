ebb85eb00d4e7ec481e57627dd8a11893baf9144
Lennox Kabo <borelennox@gmail.com>
2024-02-14 10:28:11 +0300

    header and footer files

---
+++ footer.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <footer>
+        <div class="footer-top">
+            <h2>WE CAN HELP BUILD YOUR PROJECT!</h2>
+            <h4>TELL US ABOUT YOUR PROJECT</h4>
+            <div class="footer-contact">
+                <a href="contact.php">Contact us</a>
+                <img src="assets/images/email-icon.svg" alt="email-icon">
+            </div>
+        </div>
+        <div class="footer-mid">
+            <div class="footer-deets">
+                <h2>ADDRESS</h2>
+                <p>Nachu Plaza, Kiambere Road Upper Hill,Nairobi, Kenya</p>
+            </div>
+            <div class="footer-deets">
+                <h2>PHONE</h2>
+                <p>+254 721 175 735  <br/>+254 708 900 144</p>
+            </div>
+            <div class="footer-deets">
+                <h2>EMAIL</h2>
+                <p>admin@chacleat.com</p>
+            </div>
+            <div class="footer-deets">
+                <h2>FOLLOW US</h2>
+                <div class="socials">
+                    <div class="social-icon"><a href="#"><img src="assets/images/facebook.svg" alt="facebook-icon"></a></div>
+                    <div class="social-icon"><a href="#"><img src="assets/images/instagram.svg" alt="instagram-icon"></a></div>
+                    <div class="social-icon"><a href="#"><img src="assets/images/twitter.svg" alt="twitter-icon"></a></div>
+                    <div class="social-icon"><a href="#"><img src="assets/images/linkedin.svg" alt="linkedin-icon"></a></div>
+                    <div class="social-icon"><a href="#"><img src="assets/images/pinterest.svg" alt="pinterest-icon"></a></div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <p>© 2024 All Rights Reserved. Developed by FutureTech</p>
+        </div>
+    </footer>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>header</title>
+</head>
+<body>
+    <nav>
+        <div class="logo-container">
+            <img class="logo" src="assets/images/logo.png" alt="chacleat-logo">
+        </div>
+        <ul>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="portfolio.php">Portfolio</a></li>
+            <li><a href="testimonials.php">Testimonials</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
