5323bd3e98bbc06038baea0a95c502dfc56619a9
Raymond Macharia <muthui816@gmail.com>
2024-02-15 16:00:36 +0300

    Update the portfolio page to showcase projects better

--- portfolio.php
+++ portfolio.php
@@ -1,44 +1,102 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport", content="width=device-width, initial-scale=1.0">
-        <title>Portfolio</title>
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-        <section>
-        <?php
-            include 'header.php'
-        ?>
-        </section>
-        <div class="header-text">
-            <h1>PORTFOLIO</h1>
-            <p>BROWSE THROUGH SOME OF OUR MASTERPIECES</p>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section>
+        <?php include 'header.php' ?>
+    </section>
+    <div class="header-text">
+        <h1>PORTFOLIO</h1>
+        <p>BROWSE THROUGH SOME OF OUR MASTERPIECES</p>
+    </div>
+    <div class="image-container">
+        
+        <div class="modal">
+            <span class="modal-close" onclick="closeModal()">&times;</span>
+            <img class="zoomable" src="assets/images/home-kitchen-remodel.jpg" alt="">
         </div>
-        <div class="image-container">
-            <img class="zoomable" src="assets/images/AdobeStock_1.png" alt="">
-            <img class="zoomable" src="assets/images/AdobeStock_1.png" alt="">
-            <img class="zoomable" src="assets/images/AdobeStock_1.png" alt="">
-            <img class="zoomable" src="assets/images/AdobeStock_1.png" alt="">
-            <img class="zoomable" src="assets/images/AdobeStock_1.png" alt="">
-            <img class="zoomable" src="assets/images/AdobeStock_1.png" alt="">
+        <div class="single-image">
+            <img class="zoomable" src="assets/images/home-kitchen-remodel.jpg" alt="">
+            <p>Description for image</p>
         </div>
-        <section>
-            <?php
-                include 'footer.php'
-            ?>
-        </section>
-    </body>
-</html>
+        
+        <div class="modal">
+            <span class="modal-close" onclick="closeModal()">&times;</span>
+            <img class="zoomable" src="assets/images/home-kitchen-remodel.jpg" alt="">
+        </div>
+        <div class="single-image">
+            <img class="zoomable" src="assets/images/home-kitchen-remodel.jpg" alt="">
+            <p>Description for image</p>
+        </div>
+        
+        <div class="modal">
+            <span class="modal-close" onclick="closeModal()">&times;</span>
+            <img class="zoomable" src="assets/images/home-kitchen-remodel.jpg" alt="">
+        </div>
+        <div class="single-image">
+            <img class="zoomable" src="assets/images/home-kitchen-remodel.jpg" alt="">
+            <p>Description for image</p>
+        </div>
+        
+        <div class="modal">
+            <span class="modal-close" onclick="closeModal()">&times;</span>
+            <img class="zoomable" src="assets/images/home-kitchen-remodel.jpg" alt="">
+        </div>
+        <div class="single-image">
+            <img class="zoomable" src="assets/images/home-kitchen-remodel.jpg" alt="">
+            <p>Description for image</p>
+        </div>
+        
+        <div class="modal">
+            <span class="modal-close" onclick="closeModal()">&times;</span>
+            <img class="zoomable" src="assets/images/home-kitchen-remodel.jpg" alt="">
+        </div>
+        <div class="single-image">
+            <img class="zoomable" src="assets/images/home-kitchen-remodel.jpg" alt="">
+            <p>Description for image</p>
+        </div>
+        
+        <div class="modal">
+            <span class="modal-close" onclick="closeModal()">&times;</span>
+            <img class="zoomable" src="assets/images/home-kitchen-remodel.jpg" alt="">
+        </div>
+        <div class="single-image">
+            <img class="zoomable" src="assets/images/home-kitchen-remodel.jpg" alt="">
+            <p>Description for image</p>
+        </div>
+    </div>
+    <section>
+        <?php include 'footer.php' ?>
+    </section>
 
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        const zoomableImages = document.querySelectorAll('.zoomable');
-        zoomableImages.forEach(function(image) {
-            image.addEventListener('click', function() {
-                image.classList.toggle('zoomed');
+    <script>
+        document.addEventListener("DOMContentLoaded", function () {
+        const singleImages = document.querySelectorAll(".single-image");
+        const modal = document.querySelector(".modal");
+        const modalImage = modal.querySelector("img");
+
+        singleImages.forEach(function (image) {
+            image.addEventListener("click", function () {
+            const imageSrc = this.querySelector(".zoomable").src;
+            modalImage.src = imageSrc;
+            modal.style.display = "block";
+            document.body.style.overflow = "hidden";
             });
         });
-    });
-</script>
+
+        const modalClose = document.querySelector(".modal-close");
+        modalClose.addEventListener("click", closeModal);
+
+        function closeModal() {
+            modal.style.display = "none";
+            document.body.style.overflow = "auto";
+        }
+        });
+    </script>
+</body>
+</html>
