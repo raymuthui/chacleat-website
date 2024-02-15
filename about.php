9721d9b4634467069c796ebc39ea7ed2d880e46f
Bryium Onyancha <bryiumonyancha@gmail.com>
2024-02-15 17:53:24 +0300

    Replace the images with placeholders

--- about.php
+++ about.php
@@ -1,64 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Shiv Construction Company</title>
-    <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
-    <header>
-      <?php include_once 'header.php' ?>
-    </header>
-    <div class="top">
-      <div class="image-wrapper">
-        <img src="img/jamii.jpg" alt="heading" />
-        <h1>Welcome to Shiv Construction Co.</h1>
-      </div>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Shiv Construction Company</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+  <header>
+    <?php include_once 'header.php' ?>
+  </header>
+  <div class="top">
+    <div class="image-wrapper">
+      <img src="img/jamii.jpg" alt="heading" />
+      <h1>Welcome to Chacleat Construction Co.</h1>
     </div>
-    <section>
-      <h1>Building Kenya Since 1974</h1>
+  </div>
+  <section>
+    <h1>Building Kenya Since 1974</h1>
+  </section>
+  <main>
+    <section class="about">
+      <h2>About Us</h2>
+      <p>
+        Shiv Construction Company Limited has over forty years’ experience in
+        the building and construction industry in Kenya. We are renowned
+        specialists in various areas, including housing schemes, universities
+        and student centers, hospitals, banks, office blocks, factories, and
+        civil works.
+      </p>
     </section>
-    <main>
-      <section class="about">
-        <h2>About Us</h2>
-        <p>
-          Shiv Construction Company Limited has over forty years’ experience in
-          the building and construction industry in Kenya. We are renowned
-          specialists in various areas, including housing schemes, universities
-          and student centers, hospitals, banks, office blocks, factories, and
-          civil works.
-        </p>
-      </section>
 
-      <section class="mission">
-        <h2>Our Mission</h2>
-        <p>
-          At Shiv Construction, we are dedicated to providing infrastructure and
-          building solutions for tomorrow’s world. We combine creative planning,
-          design, and construction with innovative engineering techniques to
-          develop the most appropriate solutions, working sustainably to
-          conserve energy, protect the environment, and build communities.
-        </p>
-      </section>
+    <section class="mission">
+      <h2>Our Mission</h2>
+      <p>
+        At Shiv Construction, we are dedicated to providing infrastructure and
+        building solutions for tomorrow’s world. We combine creative planning,
+        design, and construction with innovative engineering techniques to
+        develop the most appropriate solutions, working sustainably to
+        conserve energy, protect the environment, and build communities.
+      </p>
+    </section>
 
-      <section class="clientele">
-        <h2>Our Clientele</h2>
-        <p>
-          During every project, we recognize the importance of minimizing
-          construction time and anticipating disruptions. Our success in
-          managing these factors has been highlighted by the number of repeat
-          clients. These have included Government Ministries, Non-governmental
-          organizations (NGOs) such as USAID-Kenya, religious missions, public
-          and private companies, and local government bodies such as Municipal
-          Councils. Furthermore, we provide guarantees for our works and offer
-          maintenance services.
-        </p>
-      </section>
+    <section class="clientele">
+      <h2>Our Clientele</h2>
+      <p>
+        During every project, we recognize the importance of minimizing
+        construction time and anticipating disruptions. Our success in
+        managing these factors has been highlighted by the number of repeat
+        clients. These have included Government Ministries, Non-governmental
+        organizations (NGOs) such as USAID-Kenya, religious missions, public
+        and private companies, and local government bodies such as Municipal
+        Councils. Furthermore, we provide guarantees for our works and offer
+        maintenance services.
+      </p>
+    </section>
 
-      <footer>
-        <?php include_once 'footer.php' ?>
-      </footer>
-    </main>
-  </body>
+    <footer>
+      <?php include_once 'footer.php' ?>
+    </footer>
+  </main>
+</body>
+
 </html>
