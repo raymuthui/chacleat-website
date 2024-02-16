75d7c64f20b17d6d5dcb183817cb29a9c8db8511
Raymond Macharia <muthui816@gmail.com>
2024-02-16 15:50:33 +0300

    Added renovations and electric work to services

--- services.php
+++ services.php
@@ -20,7 +20,7 @@
       <img id="banner-image" src="assets/images/home-kitchen-remodel.jpg" alt="Banner Image" class="banner-image">
       <div class="header-text">
         <h1>SERVICES</h1>
-        <p>WE ARE A TEAM OF HIGHLY SKILLED PROFESSIONALS</p>
+        <p>HERE'S A RANGE OF SERVICES WE OFFER</p>
       </div>
   </section>
   <div class="container">
@@ -32,7 +32,7 @@
         </div>
         <div class="service-title">Construction</div>
         <div class="service-description">
-          Our services in construction have quite a wide scope. They range from high rise buildings, complexes, conference halls to residential premises.
+        Elevate your visions with our construction prowess, spanning high-rise buildings, complexes, conference halls, and residential premises. From the grandeur of high-rises to the comfort of homes, we build spaces that inspire and endure.
         </div>
       </div>
 
@@ -42,7 +42,7 @@
         </div>
         <div class="service-title">Road & Bridge</div>
         <div class="service-description">
-          We are experienced in a variety of road works including road furnishing, road protection works and road maintenance across classes. We also do bridge construction.
+        Mastering a spectrum of road services, we specialize in road furnishing, protection works, and maintenance across various classes. Our expertise extends to the seamless construction of bridges, ensuring connectivity and durability in every project we undertake.
         </div>
       </div>
 
@@ -52,8 +52,7 @@
         </div>
         <div class="service-title">Water, Drainage & Sewage</div>
         <div class="service-description">
-          Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind
-          texts. Separated they live in Bookmarksgrove.
+        Transforming environments with precision, we excel in water, drainage, and sewerage solutions. Our dedicated team delivers reliable services, ensuring efficient management of essential utilities for your peace of mind.
         </div>
       </div>
 
@@ -63,7 +62,27 @@
         </div>
         <div class="service-title">Solar Works</div>
         <div class="service-description">
-          As the world continues to embrace green energy, so have we at Chacleat. We do solar installations for our clients who would like to lower their power costs while preserving the environment.
+        Embracing the global shift towards green energy, we specialize in solar installations for clients seeking to reduce power costs while championing environmental preservation. Illuminate your path to sustainability with our tailored solar solutions.
+        </div>
+      </div>
+
+      <div class="service">
+        <div class="service-img">
+          <img src="assets/images/Renovation.PNG" alt="Renovation Icon">
+        </div>
+        <div class="service-title">Renovation</div>
+        <div class="service-description">
+        Escape the monotony of everyday life and rediscover the joy of homecoming! At Chacleat, we specialize in rekindling the spark through expert renovations. Let us transform your space into a haven you'll eagerly anticipate returning to every day.
+        </div>
+      </div>
+
+      <div class="service">
+        <div class="service-img">
+          <img src="assets/images/electic-works.png" alt="Electic-works Icon">
+        </div>
+        <div class="service-title">Electric</div>
+        <div class="service-description">
+        Empowering your spaces with expertise, we specialize in comprehensive electrical solutions. From installations to maintenance, our skilled team ensures seamless electrical works for a brighter and safer environment.
         </div>
       </div>
 
