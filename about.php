fe6a742850ba8e0043d6a18e2801757a4c6dd8bc
Raymond Macharia <muthui816@gmail.com>
2024-02-16 17:23:50 +0300

    Style About page

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Shiv Construction Company</title>
+  <title>About Us</title>
   <link rel="stylesheet" href="style.css" />
 </head>
 
@@ -17,37 +17,35 @@
     <img id="banner-image" src="assets/images/home-kitchen-remodel.jpg" alt="Banner Image" class="banner-image">
     <div class="header-text">
       <h1>Welcome to Chacleat Construction Co.</h1>
+      <p>Building Kenya Since 1974</p>
     </div>
-  </section>
-  <section>
-    <h1>Building Kenya Since 1974</h1>
   </section>
   <main>
     <section class="about-content">
       <section class="about">
-        <h2>About Us</h2>
-        <p>
+        <div class="service-title">About Us</div>
+        <div class="service-description">
           Chacleat Construction Company boasts more than four decades of expertise in Kenya's building and construction
           sector. Recognized as leading specialists, we've undertaken diverse projects, including housing developments,
           university facilities, medical centers, financial institutions, corporate complexes, industrial plants, and
           civil engineering initiatives.
-        </p>
+        </div>
       </section>
 
       <section class="mission">
-        <h2>Our Mission</h2>
-        <p>
+        <div class="service-title">Our Mission</div>
+        <div class="service-description">
           Certainly, here's a rewritten motto for Chacleat Construction Company:
 
           At Chacleat Construction, our mission is to craft tomorrow's urban landscape. We fuse imaginative design,
           precise engineering, and sustainable building practices to forge tailored solutions, prioritizing energy
           efficiency, environmental stewardship, and community enrichment in every project we undertake.
-        </p>
+          </div>
       </section>
 
       <section class="clientele">
-        <h2>Our Clientele</h2>
-        <p>
+        <div class="service-title">Our Clientele</div>
+        <div class="service-description">
           At Chacleat Construction Company, we prioritize the reduction of construction duration and the proactive
           management of potential disruptions in every project. Our proficiency in handling these aspects is evidenced
           by
@@ -57,7 +55,7 @@
           municipalities.
           Moreover, we assure the quality of our work through guarantees and extend our services to include maintenance
           provisions.
-        </p>
+          </div>
       </section>
     </section>
 
