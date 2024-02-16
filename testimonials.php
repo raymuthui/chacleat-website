8781d1e4380770e572243a29375a8b2c67053bb9
Bryium Onyancha <bryiumonyancha@gmail.com>
2024-02-16 15:37:21 +0300

    testimonials modification final

--- testimonials.php
+++ testimonials.php
@@ -1,96 +1,129 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>6 Text Blocks with Images and Titles</title>
-    <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
-    <header>
-      <?php include_once 'header.php' ?>
-    </header>
-    <div class="top">
-      <div class="image-wrapper">
-        <img src="assets/images/Heading.jpg" alt="heading" />
-        <h1>TESTIMONIALS</h1>
-        <p id="heading">We are a team of skilled professionals</p>
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>6 Text Blocks with Images and Titles</title>
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+  <header>
+    <?php include_once 'header.php' ?>
+  </header>
+  <div class="top">
+    <div class="image-wrapper">
+      <img src="assets/images/Heading.jpg" alt="heading" />
+      <h1>TESTIMONIALS</h1>
+      <p id="heading">We are a team of skilled professionals</p>
+    </div>
+  </div>
+  <div class="all-testimonials">
+    <div class="testimonial-container">
+      <div class="avatar">
+        <img src="assets/images/user.svg" alt="Person 1" />
+      </div>
+      <div class="profile-details">
+        <h2>Jane Doe</h2>
+        <h5>Company Owner</h5>
+      </div>
+      <div class="testimonials">
+        Even the all-powerful Pointing has no control about the blind texts it
+        is an almost unorthographic life One day however a small line of blind
+        text by the name of Lorem Ipsum decided to leave for the far World of
+        Grammar. The Big Oxmox advised her not to do so, because there were
+        thousands of bad Commas, wild Question Marks and devious Semikoli, but
+        the Little Blind Text didn’t listen..
       </div>
     </div>
-    <div class="test-container">
-      <div class="text-block">
-        <img src="assets/images/img 1.jpg" alt="Person 1" />
-        <h2>Company Owner</h2>
-        <p>
-          Even the all-powerful Pointing has no control about the blind texts it
-          is an almost unorthographic life One day however a small line of blind
-          text by the name of Lorem Ipsum decided to leave for the far World of
-          Grammar. The Big Oxmox advised her not to do so, because there were
-          thousands of bad Commas, wild Question Marks and devious Semikoli, but
-          the Little Blind Text didn’t listen..
-        </p>
+    <div class="testimonial-container">
+      <div class="avatar">
+        <img src="assets/images/user.svg" alt="Person 2" />
       </div>
-      <div class="text-block">
-        <img src="assets/images/img 2.jpg" alt="Person 2" />
-        <h3>Home Owner</h3>
-        <p>
-          Even the all-powerful Pointing has no control about the blind texts it
-          is an almost unorthographic life One day however a small line of blind
-          text by the name of Lorem Ipsum decided to leave for the far World of
-          Grammar. The Big Oxmox advised her not to do so, because there were
-          thousands of bad Commas, wild Question Marks and devious Semikoli, but
-          the Little Blind Text didn’t listen..
-        </p>
+
+      <div class="profile-details">
+        <h2>Jane Doe</h2>
+        <h5>Company Owner</h5>
       </div>
-      <div class="text-block">
-        <img src="assets/images/img 3.jpg" alt="Person 3" />
-        <h4>CEO</h4>
-        <p>
-          Even the all-powerful Pointing has no control about the blind texts it
-          is an almost unorthographic life One day however a small line of blind
-          text by the name of Lorem Ipsum decided to leave for the far World of
-          Grammar. The Big Oxmox advised her not to do so, because there were
-          thousands of bad Commas, wild Question Marks and devious Semikoli, but
-          the Little Blind Text didn’t listen..
-        </p>
-      </div>
-      <div class="text-block">
-        <img src="assets/images/img 4.jpg" alt="Person 4" />
-        <h5>Bussiness Employee</h5>
-        <p>
-          Even the all-powerful Pointing has no control about the blind texts it
-          is an almost unorthographic life One day however a small line of blind
-          text by the name of Lorem Ipsum decided to leave for the far World of
-          Grammar. The Big Oxmox advised her not to do so, because there were
-          thousands of bad Commas, wild Question Marks and devious Semikoli, but
-          the Little Blind Text didn’t listen..
-        </p>
-      </div>
-      <div class="text-block">
-        <img src="assets/images/img 5.jpg" alt="Person 5" />
-        <h6>Company Owner</h6>
-        <p>
-          Even the all-powerful Pointing has no control about the blind texts it
-          is an almost unorthographic life One day however a small line of blind
-          text by the name of Lorem Ipsum decided to leave for the far World of
-          Grammar. The Big Oxmox advised her not to do so, because there were
-          thousands of bad Commas, wild Question Marks and devious Semikoli, but
-          the Little Blind Text didn’t listen..
-        </p>
-      </div>
-      <div class="text-block">
-        <img src="assets/images/img 6.jpg" alt="Person 6" />
-        <h7>CEO</h7>
-        <p>
-          Even the all-powerful Pointing has no control about the blind texts it
-          is an almost unorthographic life One day however a small line of blind
-          text by the name of Lorem Ipsum decided to leave for the far World of
-          Grammar. The Big Oxmox advised her not to do so, because there were
-          thousands of bad Commas, wild Question Marks and devious Semikoli, but
-          the Little Blind Text didn’t listen.
-        </p>
+      <div class="testimonials">
+        Even the all-powerful Pointing has no control about the blind texts it
+        is an almost unorthographic life One day however a small line of blind
+        text by the name of Lorem Ipsum decided to leave for the far World of
+        Grammar. The Big Oxmox advised her not to do so, because there were
+        thousands of bad Commas, wild Question Marks and devious Semikoli, but
+        the Little Blind Text didn’t listen..
       </div>
     </div>
+    <div class="testimonial-container">
+      <div class="avatar">
+        <img src="assets/images/user.svg" alt="Person 3" />
+      </div>
+      <div class="profile-details">
+        <h2>Jane Doe</h2>
+        <h5>Company Owner</h5>
+      </div>
+      <div class="testimonials">
+        Even the all-powerful Pointing has no control about the blind texts it
+        is an almost unorthographic life One day however a small line of blind
+        text by the name of Lorem Ipsum decided to leave for the far World of
+        Grammar. The Big Oxmox advised her not to do so, because there were
+        thousands of bad Commas, wild Question Marks and devious Semikoli, but
+        the Little Blind Text didn’t listen..
+      </div>
+    </div>
+    <div class="testimonial-container">
+      <div class="avatar">
+        <img src="assets/images/user.svg" alt="Person 4" />
+      </div>
+      <div class="profile-details">
+        <h2>Jane Doe</h2>
+        <h5>Company Owner</h5>
+      </div>
+      <div class="testimonials">
+        Even the all-powerful Pointing has no control about the blind texts it
+        is an almost unorthographic life One day however a small line of blind
+        text by the name of Lorem Ipsum decided to leave for the far World of
+        Grammar. The Big Oxmox advised her not to do so, because there were
+        thousands of bad Commas, wild Question Marks and devious Semikoli, but
+        the Little Blind Text didn’t listen..
+      </div>
+    </div>
+    <div class="testimonial-container">
+      <div class="avatar">
+        <img src="assets/images/user.svg" alt="Person 5" />
+      </div>
+      <div class="profile-details">
+        <h2>Jane Doe</h2>
+        <h5>Company Owner</h5>
+      </div>
+      <div class="testimonials">
+        Even the all-powerful Pointing has no control about the blind texts it
+        is an almost unorthographic life One day however a small line of blind
+        text by the name of Lorem Ipsum decided to leave for the far World of
+        Grammar. The Big Oxmox advised her not to do so, because there were
+        thousands of bad Commas, wild Question Marks and devious Semikoli, but
+        the Little Blind Text didn’t listen..
+      </div>
+    </div>
+    <div class="testimonial-container">
+      <div class="avatar">
+        <img src="assets/images/user.svg" alt="Person 6" />
+      </div>
+      <div class="profile-details">
+        <h2>Jane Doe</h2>
+        <h5>CEO</h5>
+      </div>
+      <div class="testimonials">
+        Even the all-powerful Pointing has no control about the blind texts it
+        is an almost unorthographic life One day however a small line of blind
+        text by the name of Lorem Ipsum decided to leave for the far World of
+        Grammar. The Big Oxmox advised her not to do so, because there were
+        thousands of bad Commas, wild Question Marks and devious Semikoli, but
+        the Little Blind Text didn’t listen.
+      </div>
+    </div>
+  </div>
 
   <footer>
     <?php include_once 'footer.php' ?>
