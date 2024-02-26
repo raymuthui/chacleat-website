f31df9fa585557166fbe9ab885e354d99dd29bb1
Lennox Kabo <borelennox@gmail.com>
2024-02-26 12:18:50 +0300

    adding responsiveness

--- header.php
+++ header.php
@@ -11,4 +11,12 @@
             <li><a <?php if(basename($_SERVER['PHP_SELF']) == 'about.php'){echo 'class = "active"';} ?> href="about.php">About</a></li>
             <li><a <?php if(basename($_SERVER['PHP_SELF']) == 'contact.php'){echo 'class = "active"';} ?> href="contact.php">Contact Us</a></li>
         </ul>
+        <ul class="mobi-nav">
+            <li><a <?php if(basename($_SERVER['PHP_SELF']) == 'index.php' ){echo 'class = "active"';} ?> href="index.php">Home</a></li>
+            <li><a <?php if(basename($_SERVER['PHP_SELF']) == 'services.php'){echo 'class = "active"';} ?> href="services.php">Services</a></li>
+            <li><a <?php if(basename($_SERVER['PHP_SELF']) == 'portfolio.php'){echo 'class = "active"';} ?> href="portfolio.php">Portfolio</a></li>
+            <li><a <?php if(basename($_SERVER['PHP_SELF']) == 'testimonials.php'){echo 'class = "active"';} ?> href="testimonials.php">Testimonials</a></li>
+            <li><a <?php if(basename($_SERVER['PHP_SELF']) == 'about.php'){echo 'class = "active"';} ?> href="about.php">About</a></li>
+            <li><a <?php if(basename($_SERVER['PHP_SELF']) == 'contact.php'){echo 'class = "active"';} ?> href="contact.php">Contact Us</a></li>
+        </ul>
     </nav>
